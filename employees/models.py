fb438b698550d6cefee236cde8d2962bc7c9ac52
Andrey Herrera <kahs_kevin@hotmail.com>
2018-10-03 23:24:47 -0500

    Update Employees Model

--- employees/models.py
+++ employees/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from datetime import date
 
 class Employee(models.Model):
 	name = models.TextField()
 	age = models.IntegerField()
 	email = models.EmailField()
+	username = models.CharField(max_length=15,default='0')
+	password = models.TextField(default='0000000')
+	identification = models.CharField(max_length=12, default='0')
 	title = models.CharField(max_length=50)
+	phone = models.CharField(max_length=10, default='0000000')
 	salary = models.FloatField()
-	headquarter = models.CharField(max_length=30)
+	headquarter = models.IntegerField()
 	status = models.BooleanField()
+	ip = models.CharField(max_length=15, default='0')
+	token = models.TextField(default='0')
+	operation = models.IntegerField(default=0)
+	created_at = models.DateField(("Date"), default=date.today)
+	last_login = models.DateField(("Date"), default=date.today)
 
 	def __str__(self):
 		return self.name
--- employees/serializers.py
+++ employees/serializers.py
@@ -4,4 +4,5 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Employee
-		fields = ('id', 'name', 'age', 'title', 'email' ,'salary', 'headquarter', 'status')
+		fields = ('id', 'name', 'age', 'title', 'email' ,'salary', 'headquarter', 'status', 'username', 'password',
+		 'identification', 'phone','ip', 'token', 'operation','created_at', 'last_login')
